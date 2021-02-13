2a82b3cf0811f4fc0575884a5bcf51ec6530b351
github shiory602 <suzushioufo@gmail.com>
2021-02-12 16:49:19 -0800

    add jQuery

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-ScrollReveal().reveal('.reveal', { delay: 500 });
-ScrollReveal().reveal('.reveal-h2', { delay: 2000 });
+$(document).ready(function(){
+	
+
+ScrollReveal().reveal('.reveal', { 
+	delay: 200,
+  duration: 2000,
+  origin: "bottom",
+  distance: "100px",
+  opacity: 0,
+});
+
+ScrollReveal().reveal(".reveal1", { 
+	delay: 2500,
+  duration: 3500,
+  origin: "bottom",
+  distance: "100px",
+  opacity: 0,
+});
 // ScrollReveal().reveal('.punchline', { delay: 2000 });
+
+ScrollReveal().reveal('.text-content');
+})
